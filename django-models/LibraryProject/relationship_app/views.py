3f128ebe57d84b467bbe4232035abf8c22d82887
beki-github <bereketendale9@gmail.com>
2026-02-03 17:16:52 -0300

    task1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+# Login view
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('books_list')  # redirect to books list or homepage
+        else:
+            messages.error(request, "Invalid username or password")
+    return render(request, 'relationship_app/login.html')
+
+# Logout view
+def user_logout(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
+
+# Registration view
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created successfully")
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
