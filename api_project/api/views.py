9a8bbb346e812d69b5f8a20f8cee02d44cf81cb2
beki-github <bereketendale9@gmail.com>
2026-02-04 17:18:26 -0300

    task 2

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,15 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
-    """
-    API endpoint that allows all books to be viewed.
-    """
+  
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    
     queryset = Book.objects.all()
     serializer_class = BookSerializer
